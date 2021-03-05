8294812a8623dc17c42152e11bf8f51bc3f62f07
Kristianto Lie <kreizlie@gmail.com>
2021-03-05 07:58:42 +0700

    Added home page component.

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <v-btn disabled>Home</v-btn>
+    <v-btn v-on:click="gotoAbout()">About</v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    methods: {
+      gotoAbout () {
+        this.$router.push('about')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .box {
+    background: #007a79;
+  }
+</style>
