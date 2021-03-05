79849b7505c09d174727b63cd2ec438520e0972a
Kristianto Lie <kreizlie@gmail.com>
2021-03-05 07:58:34 +0700

    Added about page

---
+++ src/components/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <v-btn v-on:click="gotoHome()">Home</v-btn>
+    <v-btn disabled>About</v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'About',
+
+    methods: {
+      gotoHome () {
+        this.$router.push('home')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .box {
+    background: #ec4f43;
+  }
+</style>
