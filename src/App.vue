27a13526f1537ca47e17a470a6bd8f41840e87ab
Kristianto Lie <kreizlie@gmail.com>
2021-03-05 07:58:02 +0700

    Added Vue application file.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <v-app>
+      <router-view></router-view>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  @import "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900";
+  @import "https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css";
+</style>
